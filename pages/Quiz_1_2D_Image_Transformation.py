a64ea1fd8004edf9a07252e86d3cdfaf287b47af
alhamdulillah <126628496+welcm2uganda@users.noreply.github.com>
2023-03-16 14:07:21 +0800

    Update Quiz_1_2D_Image_Transformation.py

--- pages/Quiz_1_2D_Image_Transformation.py
+++ pages/Quiz_1_2D_Image_Transformation.py
@@ -12,67 +12,66 @@ def translation(img_, cols, rows):
     m_translation_ = np.float32([[1, 0, 100],
                                  [0, 1, 50],
                                  [0, 0, 1]])
-        img_ = Image.open(img_)
-        cols, rows = img_.shape[:2]
+    img_ = Image.open(img_)
+    cols, rows = img_.shape[:2]
 
-        translated_img_ = cv2.warpPerspective(img_, m_translation_, (cols, rows))
-        plt.axis('off')
-        plt.imshow(translated_img_)
-        plt.show()
+    translated_img_ = cv2.warpPerspective(img_, m_translation_, (cols, rows))
+    plt.axis('off')
+    plt.imshow(translated_img_)
+    plt.show()
 
 def rotation(img_, cols, rows):
     angle = np.radians(10)
     m_rotation_ = np.float32([[np.cos(angle), -(np.sin(angle)), 0],
                               [np.sin(angle), np.cos(angle), 0],
                               [0, 0, 1]])
-        img_ = Image.open(img_)
-        cols, rows = img_.shape[:2]
+    img_ = Image.open(img_)
+    cols, rows = img_.shape[:2]
 
-        rotated_img_ = cv2.warpPerspective(img_, m_rotation_, (int(cols), int(rows)))
-        plt.axis('off')
-        plt.imshow(rotated_img_)
-        plt.show()
+    rotated_img_ = cv2.warpPerspective(img_, m_rotation_, (int(cols), int(rows)))
+    plt.axis('off')
+    plt.imshow(rotated_img_)
+    plt.show()
 
 def scaling(img_, cols, rows):
     m_scaling_ = np.float32([[1.5, 0, 0],
                              [0, 1.8, 0],
                              [0, 0, 1]])
-        img_ = Image.open(img_)
-        cols, rows = img_.shape[:2]
+    img_ = Image.open(img_)
+    cols, rows = img_.shape[:2]
 
-        scaled_img_ = cv2.warpPerspective(img_, m_scaling_, (cols*2, rows*2))
-        plt.axis('off')
-        plt.imshow(scaled_img_)
-        plt.show()
+    scaled_img_ = cv2.warpPerspective(img_, m_scaling_, (cols*2, rows*2))
+    plt.axis('off')
+    plt.imshow(scaled_img_)
+    plt.show()
 
 def reflection(img_, cols, rows):
     m_reflection_ = np.float32([[1, 0, 0],
                                 [0, -1, rows],
                                 [0, 0, 1]])
-        img_ = Image.open(img_)
-        cols, rows = img_.shape[:2]
+    img_ = Image.open(img_)
+    cols, rows = img_.shape[:2]
 
-        reflected_img_ = cv2.warpPerspective(img_, m_reflection_, (int(cols), int(rows)))
-        plt.axis('off')
-        plt.imshow(reflected_img_)
-        plt.show()    
+    reflected_img_ = cv2.warpPerspective(img_, m_reflection_, (int(cols), int(rows)))
+    plt.axis('off')
+    plt.imshow(reflected_img_)
+    plt.show()    
 
 def shear(img_, cols, rows):
     m_shearing_x = np.float32([[1, 0.5, 0],
                                [0, 1, 0],
                                [0, 0, 1]])
-        img_ = Image.open(img_)
-        cols, rows = img_.shape[:2]
+    img_ = Image.open(img_)
+    cols, rows = img_.shape[:2]
 
-        sheared_img_x = cv2.warpPerspective(img_, m_shearing_x, (int(cols*1.5), int(rows*1.5)))
-        plt.axis('off')
-        plt.imshow(sheared_img_x)
-        plt.show()
+    sheared_img_x = cv2.warpPerspective(img_, m_shearing_x, (int(cols*1.5), int(rows*1.5)))
+    plt.axis('off')
+    plt.imshow(sheared_img_x)
+    plt.show()
 
 def main():
     Transformation = st.multiselect('Choose Transformation Method', ['translation', 'rotation', 'scale', 'shear', 'reflection'])
-    image_upload = st.file_uploader('Upload Image to Use', ['jpg'], accept_multiple_files=False)
-    
+    image_upload = st.file_uploader('Upload Image to Use', ['jpg'], accept_multiple_files=False)   
     if ('translation' in Transformation):
         translation(img_, cols, rows)
     if ('rotation' in Transformation):
