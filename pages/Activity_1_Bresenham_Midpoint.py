592c22ed1d2ff4e953be7687aebcc9f41f16bf34
alhamdulillah <126628496+welcm2uganda@users.noreply.github.com>
2023-03-16 18:57:32 +0800

    Update Activity_1_Bresenham_Midpoint.py

--- pages/Activity_1_Bresenham_Midpoint.py
+++ pages/Activity_1_Bresenham_Midpoint.py
@@ -14,7 +14,7 @@ def BresenhamLine(x1, y1, x2, y2, color):
     y3 = (y2 + y1) / 2
     if y2 > x2:
         x3 = (x2 - x2) / 2
-        y3 = (y2 - x2) / 2
+        y3 = (y2 - y2) / 2
    
 
     if gradient > 1:
